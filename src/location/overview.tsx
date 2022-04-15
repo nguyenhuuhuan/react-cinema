1b4aa1cf22d3aa9e2c685347ca7d4342039b49e4
thanhhaile051020 <thanhhaile0510@gmail.com>
2022-04-15 15:36:19 +0700

    update review

--- src/location/overview.tsx
+++ src/location/overview.tsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom'
 
 export const Overview = () => {
     const location = useLocation()
-    if (location.pathname.split('/').length == 3)
+    if (location.pathname.split('/').length === 3)
         return (
             <div className='row top-content'>
                 <div className='col s4 m5 l12 summary' >Overview</div>
