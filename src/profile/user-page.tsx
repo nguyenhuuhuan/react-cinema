45ba18d2617698a1c41b487831d61903a7ad44c4
vinhtq2020 <quangvinh2015vi@gmail.com>
2022-05-04 14:04:00 +0700

    update file user-page.tsx

--- src/profile/user-page.tsx
+++ src/profile/user-page.tsx
@@ -10,8 +10,8 @@ export function UserPage() {
   const [user, setUser] = useState<User>({} as User);
   const refForm = useRef();
   useEffect(() => {
-    const id = buildId<string>(params)||'';    
-    getMyProfileService().getMyProfile(id).then(usr => {      
+    const id = buildId<string>(params) || '';
+    getMyProfileService().getMyProfile(id).then(usr => {
       if (usr) {
         setUser(usr);
       }
@@ -50,22 +50,23 @@ export function UserPage() {
         </header>
         <div className='row'>
           <div className='col m12 l4'>
-            <div className='card'>
+            {(user.occupation || user.company) && <div className='card'>
               <header>
                 <i className='material-icons highlight'>account_box</i>
                 {resource.user_profile_basic_info}
               </header>
-              <p>{user.occupation}</p>
-              <p>{user.company}</p>
+              {user.occupation && <p>{user.occupation}</p>}
+              {user.company && <p>{user.company}</p>}
             </div>
-            <div className='card'>
+}
+            {user.skills && user.skills.length > 0 && <div className='card'>
               <header>
                 <i className='material-icons highlight'>local_mall</i>
                 {resource.skills}
               </header>
               <section>
                 {
-                  user.skills && user.skills.map((item, index) => {
+                  user.skills.map((item, index) => {
                     return <p key={index}>{item.skill}<i hidden={!item.hirable} className='star highlight' /></p>;
                   })
                 }
@@ -75,144 +76,151 @@ export function UserPage() {
                   {resource.user_profile_hireable_skill}
                 </p>
               </section>
-            </div>
-            <div className='card'>
+            </div>}
+            {user.lookingFor && user.lookingFor.length > 0 && <div className='card'>
               <header>
                 <i className='material-icons highlight'>find_in_page</i>
                 {resource.user_profile_looking_for}
               </header>
               <section>
                 {
-                  user.lookingFor && user.lookingFor.map((item, index) => {
+                  user.lookingFor.map((item, index) => {
                     return (<p key={index}>{item}</p>);
                   })
                 }
               </section>
             </div>
-            <div className='card'>
-              <header>
-                <i className='material-icons highlight'>chat</i>
-                {resource.user_profile_social}
-              </header>
-              <div>
-                {
-                  user.facebookLink &&
-                  <a href={'https://facebookcom/' + user.facebookLink} title='facebook' target='_blank' rel='noreferrer'>
-                    <i className='fa fa-facebook' />
-                    <span>facebook</span>
-                  </a>
-                }
-                {
-                  user.skypeLink &&
-                  <a href={'https://skype.com/' + user.skypeLink} title='Skype' target='_blank' rel='noreferrer'>
-                    <i className='fa fa-skype' />
-                    <span>Skype</span>
-                  </a>
-                }
-                {
-                  user.twitterLink &&
-                  <a href={'https://twitter.com/' + user.twitterLink} title='Twitter' target='_blank' rel='noreferrer'>
-                    <i className='fa fa-twitter' />
-                    <span>Twitter</span>
-                  </a>
-                }
-                {
-                  user.instagramLink &&
-                  <a href={'https://instagram.com/' + user.instagramLink} title='Instagram' target='_blank' rel='noreferrer'>
-                    <i className='fa fa-instagram' />
-                    <span>Instagram</span>
-                  </a>
-                }
-                {
-                  user.linkedinLink &&
-                  <a href={'https://linkedin.com/' + user.linkedinLink} title='Linked in' target='_blank' rel='noreferrer'>
-                    <i className='fa fa-linkedin' />
-                    <span>Linked in</span>
-                  </a>
-                }
-                {
-                  user.googleLink &&
-                  <a href={'https://plus.google.com/' + user.googleLink} title='Google' target='_blank' rel='noreferrer'>
-                    <i className='fa fa-google' />
-                    <span>Google</span>
-                  </a>
-                }
-                {
-                  user.dribbbleLink &&
-                  <a href={'https://dribbble.com/' + user.dribbbleLink} title='dribbble' target='_blank' rel='noreferrer'>
-                    <i className='fa fa-dribbble' />
-                    <span>dribbble</span>
-                  </a>
-                }
+            }
+            {(user.facebookLink || user.skypeLink || user.twitterLink || user.instagramLink
+              || user.linkedinLink || user.googleLink || user.dribbbleLink || user.customLink01
+              || user.customLink02 || user.customLink03 || user.customLink04 || user.customLink05
+              || user.customLink06 || user.customLink07 || user.customLink08) &&
+              <div className='card'>
+                <header>
+                  <i className='material-icons highlight'>chat</i>
+                  {resource.user_profile_social}
+                </header>
+                <div>
+                  {
+                    user.facebookLink &&
+                    <a href={'https://facebookcom/' + user.facebookLink} title='facebook' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-facebook' />
+                      <span>facebook</span>
+                    </a>
+                  }
+                  {
+                    user.skypeLink &&
+                    <a href={'https://skype.com/' + user.skypeLink} title='Skype' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-skype' />
+                      <span>Skype</span>
+                    </a>
+                  }
+                  {
+                    user.twitterLink &&
+                    <a href={'https://twitter.com/' + user.twitterLink} title='Twitter' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-twitter' />
+                      <span>Twitter</span>
+                    </a>
+                  }
+                  {
+                    user.instagramLink &&
+                    <a href={'https://instagram.com/' + user.instagramLink} title='Instagram' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-instagram' />
+                      <span>Instagram</span>
+                    </a>
+                  }
+                  {
+                    user.linkedinLink &&
+                    <a href={'https://linkedin.com/' + user.linkedinLink} title='Linked in' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-linkedin' />
+                      <span>Linked in</span>
+                    </a>
+                  }
+                  {
+                    user.googleLink &&
+                    <a href={'https://plus.google.com/' + user.googleLink} title='Google' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-google' />
+                      <span>Google</span>
+                    </a>
+                  }
+                  {
+                    user.dribbbleLink &&
+                    <a href={'https://dribbble.com/' + user.dribbbleLink} title='dribbble' target='_blank' rel='noreferrer'>
+                      <i className='fa fa-dribbble' />
+                      <span>dribbble</span>
+                    </a>
+                  }
 
-                {
-                  user.customLink01 &&
-                  <a href={user.customLink01} target='_blank' rel='noreferrer'>
-                    <i className='fab fa-globe-asia' />
-                  </a>
-                }
-                {
-                  user.customLink02 &&
-                  <a href={user.customLink02} target='_blank' rel='noreferrer'>
-                    <i className='fab fa-globe-asia' />
-                  </a>
-                }
-                {
-                  user.customLink03 && <a href={user.customLink03} target='_blank' rel='noreferrer'>
-                    <i className='fab fa-globe-asia' />
-                  </a>
-                }
-                {
-                  user.customLink04 && <a href={user.customLink04} target='_blank' rel='noreferrer'>
-                    <i className='fab fa-globe-asia' />
-                  </a>
-                }
-                {
-                  user.customLink05 && <a href={user.customLink05} target='_blank' rel='noreferrer'>
-                    <i className='fab fa-globe-asia' />
-                  </a>
-                }
-                {
-                  user.customLink06 && <a href={user.customLink06} target='_blank' rel='noreferrer'>
-                    <i className='fab fa-globe-asia' />
-                  </a>
-                }
-                {
-                  user.customLink07 && <a href={user.customLink07} target='_blank' rel='noreferrer'>
-                    <i className='fab fa-globe-asia' />
-                  </a>
-                }
-                {
-                  user.customLink08 && <a href={user.customLink08} target='_blank' rel='noreferrer'>
-                    <i className='fab fa-globe-asia' />
-                  </a>
-                }
+                  {
+                    user.customLink01 &&
+                    <a href={user.customLink01} target='_blank' rel='noreferrer'>
+                      <i className='fab fa-globe-asia' />
+                    </a>
+                  }
+                  {
+                    user.customLink02 &&
+                    <a href={user.customLink02} target='_blank' rel='noreferrer'>
+                      <i className='fab fa-globe-asia' />
+                    </a>
+                  }
+                  {
+                    user.customLink03 && <a href={user.customLink03} target='_blank' rel='noreferrer'>
+                      <i className='fab fa-globe-asia' />
+                    </a>
+                  }
+                  {
+                    user.customLink04 && <a href={user.customLink04} target='_blank' rel='noreferrer'>
+                      <i className='fab fa-globe-asia' />
+                    </a>
+                  }
+                  {
+                    user.customLink05 && <a href={user.customLink05} target='_blank' rel='noreferrer'>
+                      <i className='fab fa-globe-asia' />
+                    </a>
+                  }
+                  {
+                    user.customLink06 && <a href={user.customLink06} target='_blank' rel='noreferrer'>
+                      <i className='fab fa-globe-asia' />
+                    </a>
+                  }
+                  {
+                    user.customLink07 && <a href={user.customLink07} target='_blank' rel='noreferrer'>
+                      <i className='fab fa-globe-asia' />
+                    </a>
+                  }
+                  {
+                    user.customLink08 && <a href={user.customLink08} target='_blank' rel='noreferrer'>
+                      <i className='fab fa-globe-asia' />
+                    </a>
+                  }
+                </div>
               </div>
-            </div>
+            }
           </div>
           <div className='col m12 l8'>
-            <div className='card border-bottom-highlight'>
+            {user.bio && user.bio.length >0 && <div className='card border-bottom-highlight'>
               <header>
                 <i className='material-icons highlight'>person</i>
                 {resource.user_profile_bio}
               </header>
               <p>{user.bio}</p>
             </div>
-            <div className='card border-bottom-highlight'>
+}
+            {user.interests && user.interests.length >0 && <div className='card border-bottom-highlight'>
               <header>
                 <i className='material-icons highlight'>flash_on</i>
                 {resource.interests}
               </header>
               <section className='row'>
                 {
-                  user.interests && user.interests.map((item, index) => {
+                  user.interests.map((item, index) => {
                     return (<span key={index} className='col s4'>{item}</span>);
                   })
                 }
               </section>
-            </div>
+            </div>}
 
-            <div className='card border-bottom-highlight'>
+            {user.achievements && user.achievements.length>0 &&<div className='card border-bottom-highlight'>
               <header>
                 <i className='material-icons highlight'>beenhere</i>
                 {resource.achievements}
@@ -229,6 +237,7 @@ export function UserPage() {
                 })
               }
             </div>
+}
           </div>
         </div>
       </form>
